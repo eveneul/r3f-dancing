d2b05fdaa25d989d8fc0cf3046fa12b375e8d602
오하늘 <nebulass@naver.com>
2024-01-30 14:38:33 +0900

    boxGeometry -> drei로 변경

--- src/components/Mesh.jsx
+++ src/components/Mesh.jsx
@@ -1,8 +1,14 @@
+import { Box } from "@react-three/drei";
+
 export default function Mesh() {
   return (
-    <mesh>
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={0xff0000} />
-    </mesh>
+    // <mesh>
+    //   <boxGeometry args={[1, 1, 1]} />
+    //   <meshStandardMaterial color={0xff0000} />
+    // </mesh>
+
+    <Box
+      args={[1, 1, 1]}
+      material-color={0xff0000}></Box>
   );
 }
