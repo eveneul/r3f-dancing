19da456c5225d582ad7d30b3bab96797c6387c49
오하늘 <nebulass@naver.com>
2024-01-31 14:42:38 +0900

    모델링 애니메이션 실행

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -1,8 +1,13 @@
-import { useGLTF } from "@react-three/drei";
-import { useEffect } from "react";
+import { useAnimations, useGLTF } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
+import { useEffect, useRef } from "react";
 
 export default function Modeling() {
-  const { scene } = useGLTF("/modeling/dancer.glb");
+  const { scene, animations } = useGLTF("/modeling/dancer.glb");
+  const modelRef = useRef(null);
+
+  const { actions } = useAnimations(animations, modelRef);
+
   // 모델링 그림자 생성
   useEffect(() => {
     scene.traverse((object) => {
@@ -11,10 +16,18 @@ export default function Modeling() {
         object.receiveShadow = true;
       }
     });
-  }, [scene]);
+
+    actions["twerk"].play();
+  }, [scene, actions]);
+
+  useFrame((state, delta) => {
+    // state: scene, renderer의 정보...
+    // modelRef.current.rotation.y += 0.02;
+  });
 
   return (
     <primitive
+      ref={modelRef}
       object={scene}
       scale={0.01}
       position-y={0.4}
@@ -24,4 +37,8 @@ export default function Modeling() {
 
 /**
  * primitive:: object => geomatry 즉 모델링된 요소를 넣어 주면 쉽게 mesh를 만들어 줌
+ */
+
+/**
+ * useFrame:: requestAnimationFrame Hook
  */
