963d47b68f0d240d71ccb1d1304a05b14756a30e
오하늘 <nebulass@naver.com>
2024-02-01 13:17:25 +0900

    gsap으로 camera 연동

--- src/components/Dancer.jsx
+++ src/components/Dancer.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useRef } from "react";
 import { useAnimations, useGLTF, useScroll } from "@react-three/drei";
-import { useFrame } from "@react-three/fiber";
+import { useFrame, useThree } from "@react-three/fiber";
 import { useRecoilValue } from "recoil";
 import { isEnteredState } from "../state";
 import Loader from "./Loader";
+import gsap from "gsap";
 
 function Dancer() {
   const isEntered = useRecoilValue(isEnteredState);
@@ -24,6 +25,15 @@ function Dancer() {
   useFrame(() => {
     // console.log(scroll.offset); // lenis의 scroll progress와 비슷한 개념
   });
+
+  const three = useThree();
+
+  useEffect(() => {
+    if (!isEntered || !dancerRef.current) return;
+
+    gsap.fromTo(three.camera.position, { x: -5, y: 5, z: 5 }, { x: 0, y: 6, z: 12, duration: 2.5 });
+    gsap.fromTo(three.camera.rotation, { z: Math.PI }, { z: 0, duration: 2.5 });
+  }, [gsap, isEntered, dancerRef.current]);
 
   return (
     <>
